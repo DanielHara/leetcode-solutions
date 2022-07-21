a89b2001e571a66cbe9806c8eea54a69a63d4ded
Daniel Hara <haradaniel734@gmail.com>
2022-07-21 23:06:41 +0200

    2150

---
+++ 2150.py
@@ -0,0 +1,20 @@
+# Question 1992: https://leetcode.com/problems/find-all-lonely-numbers-in-the-array/
+
+"""
+    In any question for which the order of elements in an array doesn't matter (like this one),
+    consider making a set or frequency dictionary out of it.
+"""
+
+class Solution:
+    def findLonely(self, nums: List[int]) -> List[int]:
+        freq_dict = {}
+        
+        for num in nums:
+            freq_dict[num] = freq_dict.get(num, 0) + 1
+        
+        result = []
+        for [key, freq] in freq_dict.items():
+            if freq == 1 and ((key + 1) not in freq_dict and (key - 1) not in freq_dict):
+                result.append(key)
+        
+        return result
