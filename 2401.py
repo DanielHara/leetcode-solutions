dc88b9acbbfc4b646bd291a24dec41ed14a1b74c
Daniel Hara <haradaniel734@gmail.com>
2023-05-01 21:35:51 +0200

    2401

---
+++ 2401.py
@@ -0,0 +1,64 @@
+"""
+Question 2401: https://leetcode.com/problems/longest-nice-subarray/
+
+Very interesting question, just save a set of the binary positions you've already seen and use a sliding window approach.
+"""
+
+
+class Solution:
+    def getPositions(self, num: List[int]) -> set:
+        result_set = set()
+        count = 0
+        while num > 0:
+            if num % 2 != 0:
+                result_set.add(count)
+
+            count = count + 1
+            num = num // 2
+        
+        return result_set
+    
+    
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        accumulated_set = self.getPositions(nums[0])
+
+        j = 0
+        i = 1
+        result = 1
+
+        while i < len(nums) and j < len(nums):
+            invalid_set = None
+
+            while i < len(nums):
+                new_element = nums[i]
+                new_positions = self.getPositions(new_element)
+                
+                invalid_set = set()
+
+                is_invalid = False
+                for new_position in new_positions:
+                    if new_position in accumulated_set:
+                        is_invalid = True
+                        invalid_set.add(new_position)
+                
+                if is_invalid:
+                    break
+                else:
+                    for position in new_positions:
+                        accumulated_set.add(position)
+                    i = i + 1
+                    result = max(result, i - j)
+            
+            while j < len(nums) and invalid_set:
+                removed_element = nums[j]
+                removed_positions = self.getPositions(nums[j])
+
+                for position in removed_positions:
+                    accumulated_set.remove(position)
+                    if position in invalid_set:
+                        invalid_set.remove(position)
+                
+                j = j + 1
+        
+        return result
+
