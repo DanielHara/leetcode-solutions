d728374d7e31a48caf423eeb15af0a8d83bc9acf
Daniel Hara <haradaniel734@gmail.com>
2023-06-06 23:19:49 +0200

    2703

---
+++ 2703.js
@@ -0,0 +1,17 @@
+/*
+Question 2703: https://leetcode.com/problems/return-length-of-arguments-passed/description/
+
+I actually don't know what this question is doing on Leetcode :/
+*/
+
+
+/**
+ * @return {number}
+ */
+ var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
