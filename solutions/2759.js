82f2ac3f4152d12e92ece80d5d25b0eb12fb3a2f
Daniel Hara <haradaniel734@gmail.com>
2024-08-18 10:27:55 +0200

    2759

--- solutions/2759.js
+++ solutions/2759.js
@@ -6,46 +6,71 @@
 
  /*
     Still an incomplete solution.   
-    It passes the test cases with nested JSONs, containing only strings:
+    It passes the test cases with nested JSONs, containing only strings, numbers, booleans, and null:
     '{"a":"e"}'
     '{"a":"e","b":{"c":"d"}}'
     '{"a":"e","b":{"c":{"bg":"gd"}}}'
     '{"a":"e","b":{"c":{"bg":"gd"},"tg":"avc"}}'
+    '{"a":2,"b":{"c":"d"},"c":true,"d":null}'
  */
-    var jsonParse = function(str) {
-        const tokenizedStr = str.split(/({|}|}|:|,)/g)
-        const tokens = tokenizedStr.filter(str => str.length > 0 && str !== ',');
-    
-        const stack = [];
-    
-        for (const token of tokens) {
-            if (token === '{') {
-                stack.push({});
-            } else if (token === '}') {
-                if (stack.length >= 4) {
-                    const currentObject = stack.pop();
-                    stack.pop();
-                    const key = stack.pop().replaceAll('"', '');
-                    const object = stack.pop();
-                    object[key] = currentObject
-                    stack.push(object);
-                }
+
+    /**
+ * @param {string} str
+ * @return {null|boolean|number|string|Array|Object}
+ */
+
+var parsePrimitive = function(str) {
+    if (str.includes('"')) {
+        return str.replaceAll('"', '');
+    }
+
+    if (str === 'true') {
+        return true;
+    }
+
+    if (str === 'false') {
+        return false;
+    }
+
+    if (str === 'null') {
+        return null;
+    }
+
+    return Number(str);
+}
+
+var jsonParse = function(str) {
+    const tokenizedStr = str.split(/({|}|}|:|,)/g)
+    const tokens = tokenizedStr.filter(str => str.length > 0 && str !== ',');
+
+    const stack = []
+
+    for (const token of tokens) {
+        if (token === '{') {
+            stack.push({});
+        } else if (token === '}') {
+            if (stack.length >= 4) {
+                const currentObject = stack.pop();
+                stack.pop();
+                const key = stack.pop().replaceAll('"', '');
+                const object = stack.pop();
+                object[key] = currentObject
+                stack.push(object);
+            }
+        } else {
+            if (stack && stack[stack.length - 1] === ':') {
+                stack.pop();
+                const key = stack.pop().replaceAll('"', '');
+                const object = stack.pop();
+                const value = parsePrimitive(token);
+                object[key] = value;
+
+                stack.push(object)
             } else {
-                if (stack && stack[stack.length - 1] === ':') {
-                    stack.pop();
-                    const key = stack.pop().replaceAll('"', '');
-                    const object = stack.pop();
-    
-                    // Tricky if the token is a number, or a string
-                    object[key] = token.replaceAll('"', '');
-    
-                    stack.push(object)
-                } else {
-                    stack.push(token);
-                }
+                stack.push(token);
             }
         }
-    
-        return stack[0];
-    };
-    
+    }
+
+    return stack[0];
+};
