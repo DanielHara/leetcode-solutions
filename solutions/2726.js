cb8166656c27f1dea8a14a413a290e96a6e270e8
Daniel Hara <haradaniel734@gmail.com>
2025-01-01 16:55:31 -0300

    2726

---
+++ solutions/2726.js
@@ -0,0 +1,70 @@
+/*
+    Question 2726: https://leetcode.com/problems/calculator-with-method-chaining/
+
+    Trivial question
+*/
+
+class Calculator {
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+        this.result = value
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value) {
+        this.result = this.result + value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value) {
+        this.result = this.result - value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */  
+    multiply(value) {
+        this.result = this.result * value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        if (value === 0) {
+            throw 'Division by zero is not allowed'
+        }
+        
+        this.result = this.result / value;
+        return this;
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        this.result = Math.pow(this.result, value);
+        return this;
+    }
+    
+    /** 
+     * @return {number}
+     */
+    getResult() {
+        return this.result;
+    }
+}
